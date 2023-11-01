99c23dff817a1a5590ffa2f61af7d17b00074cde
Mikhail <arthasmontano@mail.ru>
2023-11-01 10:38:58 +0100

    Restored Initial setup in task E

--- quali/E/drawTree.js
+++ quali/E/drawTree.js
@@ -1,9 +1,6 @@
 function drawTree(startY, angle, level = 0) {
     const startX = canvas.width / 2;
     const len = length * Math.pow(depth, level);
-
-    console.log('len is', len)
-    console.log('len is2', -len)
     
     ctx.beginPath();
     ctx.save();
@@ -15,7 +12,6 @@ function drawTree(startY, angle, level = 0) {
 
     ctx.strokeStyle = computeColor(level);
 
-    // console.log(computeColor.toString())
     ctx.lineWidth = computeWidth(level);
 
     ctx.stroke();
@@ -28,47 +24,9 @@ function drawTree(startY, angle, level = 0) {
 
     
     const newLevel = level + 1;
-    // console.log(len, angle, angleOffset, newLevel)
-    // console.log(ctx)
 
     drawTree(-len, angle + angleOffset, newLevel);
     drawTree(-len, angle - angleOffset, newLevel);
 
     ctx.restore();
 };
-
-// function drawTree(startY, angle, level = 0) {
-//     const startX = canvas.width / 2;
-//     let y = startY
-//     let len = length * Math.pow(depth, level)
-//     let newLevel = level;
-
-//     let counter = 0;
-//     while (counter <= 1000) {
-        
-//         ctx.beginPath();
-//         ctx.save();
-        
-//         ctx.translate(newLevel ? 0 : startX, y);
-//         ctx.rotate(angle * Math.PI / 180);
-//         ctx.moveTo(0, 0);
-//         ctx.lineTo(0, -len);
-        
-//         ctx.strokeStyle = computeColor(newLevel);
-        
-//         // console.log(computeColor.toString())
-//         ctx.lineWidth = computeWidth(newLevel);
-        
-//         ctx.stroke();
-//         console.log('here' ,counter, len)
-//         newLevel = level + 1;
-//         len = length * Math.pow(depth, newLevel);
-//         ctx.restore()
-//         counter++;
-//     }
-
-    
-  
-//     console.log('here')
-//     ctx.restore();
-// };
